5f4e7ec240e44ed565177fea52ce53bc98bb071e
Rob Orsini <rob@tupleshop.com>
2024-12-22 04:33:46 +0000

    add filter products by category

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Entity\Category;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,13 +10,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
-    #[Route('/')]
-    public function show(EntityManagerInterface $entityManager): Response
+    #[Route('/{cat_id}')]
+    public function show(EntityManagerInterface $entityManager, int $cat_id = NULL): Response
     {
-
-
-        $repository = $entityManager->getRepository(Product::class);
-        $products = $repository->findAll();
+        if ($cat_id == NULL) {
+            $repository = $entityManager->getRepository(Product::class);
+            $products = $repository->findAll();
+        } else {
+            $repository = $entityManager->getRepository(Category::class);
+            $category = $repository->findOneById($cat_id);
+            $products = $category->getProducts();
+        }
 
         $total_products = count($products);
 
